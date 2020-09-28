8ff2a9cbfbaa0df7e1df87f2452face326ef8a04
JuliaFedyaeva <julia.fedyaeva@mail.ru>
2020-09-28 18:25:17 +0300

    Добавляет проверку перехода в корзину со страницы товара

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 import pytest
+from .pages.cart_page import CartPage
 
 
 @pytest.mark.parametrize('link', ["?promo=offer0",
@@ -61,3 +62,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.should_be_empty_cart()
