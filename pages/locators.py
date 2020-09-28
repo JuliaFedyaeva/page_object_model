969bbf5ab1f0ab3f8c05db59067de32b3595d225
JuliaFedyaeva <julia.fedyaeva@mail.ru>
2020-09-28 14:08:43 +0300

    Добавляет тест добавления товара со страницы товара

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,16 @@ class LoginPageLocators():
     AUTHORIZATION_FORM_EMAIL = (By.CSS_SELECTOR, '#id_login-username')
     AUTHORIZATION_FORM_PASSWORD = (By.CSS_SELECTOR, '#id_login-password')
 
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_CART = (By.CSS_SELECTOR, '#add_to_basket_form button.btn.btn-lg.btn-primary.btn-add-to-basket')
+    BUTTON_VIEW_CART = (By.PARTIAL_LINK_TEXT, 'basket')
+    PRODUCT_NAME = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/h1')
+    PRODUCT_PRICE = (By.XPATH, '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]')
+    PRODUCT_IN_SUCCESS_ADD_MESSAGE = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
+    TOTAL_PRICE_IN_SUCCESS_MESSAGE = (By.XPATH, '//*[@id="messages"]/div[3]/div/p[1]/strong')
+
+
+class CartPageLocators():
+    BUTTON_CHECKOUT = (By.PARTIAL_LINK_TEXT, 'checkout')
+    ITEM_IN_CATALOG = (By.XPATH, '//*[@id="basket_formset"]/div/div/div[2]/h3/a')
---
+++ pages/product_page.py
@@ -0,0 +1,32 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_cart(self):
+        link = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_CART)
+        link.click()
+
+    def go_to_cart(self):
+        view_link = self.browser.find_element(*ProductPageLocators.BUTTON_VIEW_CART)
+        view_link.click()
+
+    def get_name_item_from_success_message(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_IN_SUCCESS_ADD_MESSAGE).text
+
+    def get_item_price_from_success_message(self):
+        return self.browser.find_element(*ProductPageLocators.TOTAL_PRICE_IN_SUCCESS_MESSAGE).text
+
+    def get_item_name_on_product_page(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_item_price_on_product_page(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def should_be_right_item_in_success_message(self):
+        assert self.get_item_name_on_product_page() == self.get_name_item_from_success_message(), \
+            "Wrong item name in success add message"
+
+    def should_be_right_total_price_in_success_message(self):
+        assert self.get_item_price_on_product_page() == self.get_item_price_from_success_message(), \
+            "Wrong total price in success add message"
